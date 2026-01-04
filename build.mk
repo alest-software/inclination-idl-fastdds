f1580ad83e45fd62e81bc52a3542a2c29c87a686
M.J. de Wit <maarten@alest-software.nl>
2026-01-04 11:30:41 +0100

    Improvements

--- build.mk
+++ build.mk
@@ -1,39 +1,37 @@
 all: build stage control package
 
-#CMAKE_MODULE_PATH=$(PWD)/cmake;/opt/fastcdr
 CMAKE_MODULE_PATH=/opt/fastcdr
 
 build:
 	mkdir -p ./build
-	cd build; \
-	cmake -DCMAKE_MODULE_PATH="$(CMAKE_MODULE_PATH)" -DCMAKE_INSTALL_PREFIX=/opt/inclination ../src; \
-	cmake --build .
+	cd build && cmake -DCMAKE_MODULE_PATH="$(CMAKE_MODULE_PATH)" -DCMAKE_INSTALL_PREFIX=/opt/inclination ../src
+	cd build && cmake --build .
 
-PROJECT_PACKAGE_NAME = inclination-idl-fastdds
+package = inclination-idl-fastdds
 GITHUB_RUN_NUMBER ?= 0
-fastddsgen_FULL_VERSION = 0.1.0-$(GITHUB_RUN_NUMBER)
-ARCH = $(shell dpkg --print-architecture)
-STAGE_DIR = $(PROJECT_PACKAGE_NAME)_$(fastddsgen_FULL_VERSION)_$(ARCH)
+full_version = 0.1.0-$(GITHUB_RUN_NUMBER)
+architecture = $(shell dpkg --print-architecture)
+STAGE_DIR = $(package)_$(full_version)_$(architecture)
 debian_dir = $(PWD)/$(STAGE_DIR)/DEBIAN
 control_file = $(debian_dir)/control
-PROJECT_MAINTAINER = you@example.com
-PROJECT_DESCRIPTION = You @ example.com
+maintainer = you@example.com
+description = You @ example.com
 fastddsgen_STAGE_DIR = .
 fastddsgen_BINARY_PACKAGE_NAME = $(STAGE_DIR)
 
 stage:
-	cd build; DESTDIR=$(PWD)/$(STAGE_DIR) cmake --install .
+	cd build && DESTDIR=$(PWD)/$(STAGE_DIR) cmake --install .
 
 control:
 	@mkdir -p $(debian_dir)
-	@echo "Package: $(PROJECT_PACKAGE_NAME)"     > $(control_file)
-	@echo "Version: $(fastddsgen_FULL_VERSION)" >> $(control_file)
+	@echo "Package: $(package)"                  > $(control_file)
+	@echo "Version: $(full_version)"            >> $(control_file)
 	@echo "Section: devel"                      >> $(control_file)
 	@echo "Priority: optional"                  >> $(control_file)
-	@echo "Architecture: $(ARCH)"               >> $(control_file)
+	@echo "Architecture: $(architecture)"       >> $(control_file)
 	@echo "Depends: openjdk-17-jre"             >> $(control_file)
-	@echo "Maintainer: $(PROJECT_MAINTAINER)"   >> $(control_file)
-	@echo "Description: $(PROJECT_DESCRIPTION)" >> $(control_file)
+	@echo "Maintainer: $(maintainer)"           >> $(control_file)
+	@echo "Description: $(description)"         >> $(control_file)
 	@cat $(control_file)
 
 package:
